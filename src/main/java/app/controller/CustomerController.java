49bf29b61dc14d194cf61861fb6d94c4f55c7d84
Alexandr Eriman <de.eriman.de@gmail.com>
2025-12-10 10:57:09 +0200

    controller

--- src/main/java/app/controller/CustomerController.java
+++ src/main/java/app/controller/CustomerController.java
@@ -1,4 +1,49 @@
 package app.controller;
 
+import app.domain.Customer;
+import app.service.CustomerService;
+
+import java.util.List;
+
 public class CustomerController {
+    private final CustomerService service = new CustomerService();
+
+
+    public Customer save(String name) {
+        Customer customer = new Customer(name);
+        return service.save(customer);
+    }
+
+    public List<Customer> getAllActiveCustomers() {
+        return service.getAllActiveCustomers();
+    }
+
+
+    public Customer getActiveCustomerById(String id) {
+        Long numberId = Long.parseLong(id);
+        return service.getActiveCustomerById(numberId);
+    }
+
+    ;
+
+    public void deleteById(String id) {
+        Long numberId = Long.parseLong(id);
+        service.deleteById(numberId);
+    }
+
+    public void deleteByName(String name) {
+
+        service.deleteByName(name);
+    }
+    public void restoreById(String id) {
+        Long numberId = Long.parseLong(id);
+        service.restoreById(numberId);
+    }
+
+    public int getActiveCustomersNumber() {
+        return service.getActiveCustomersNumber();
+    }
+
+
+
 }
--- src/main/java/app/controller/FilmController.java
+++ src/main/java/app/controller/FilmController.java
@@ -1,4 +1,58 @@
 package app.controller;
 
+import app.domain.Film;
+import app.exceptions.FilmNotFoundException;
+import app.exceptions.FilmUpdateException;
+import app.service.FilmService;
+
+import java.util.List;
+
 public class FilmController {
+    // Имитация базы данных
+
+    private final FilmService service = FilmService.getInstance();
+
+    public Film save(String name, String rating, String year, String genre) {
+        Double ratingDouble = Double.parseDouble(rating);
+        int yearInt = Integer.parseInt(year);
+
+        Film film = new Film(name,ratingDouble,yearInt,genre);
+        return service.save(film);
+    }
+
+    //    Сохранить продукт в базе данных (при сохранении продукт автоматически считается активным).
+
+    public List<Film> getAllActiveFilm(){
+        return service.getAllActiveFilm();
+    }
+
+    public Film getActive(String id){
+        Long idLong = Long.parseLong(id);
+        return service.getActiveFilmById(idLong);
+    }
+
+    public void updateRating(String id, String rating){
+        Long idLong = Long.parseLong(id);
+        Double ratingDouble = Double.parseDouble(id);
+        service.updateRating(idLong, ratingDouble);
+    }
+
+    public void deleteById(String id){
+        Long idLong = Long.parseLong(id);
+        service.deleteById(idLong);
+    }
+
+    public void deleteByTitle(String title){
+        service.deleteByTitle(title);
+    }
+
+    public void restoreById(String id){
+        Long idLong = Long.parseLong(id);
+        service.restoreById(idLong);
+    }
+
+    public int getActiveProductsCount(){
+        return service.getAllActiveFilm().size();
+    }
+
 }
